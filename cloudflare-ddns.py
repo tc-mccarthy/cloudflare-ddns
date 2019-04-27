1b1eb21a30d3a8c57bbd0abd9d3369c6edd81443
TC McCarthy <tc.mccarthy1@gmail.com>
2019-04-27 14:00:39 -0400

    Initial commit

---
+++ cloudflare-ddns.py
@@ -0,0 +1,75 @@
+import requests
+import json
+
+with open("./config.json") as config_file:
+    config = json.loads(config_file.read())
+
+
+# Captures IPv4 and IPv6 IPs where applicable
+def getIPs():
+    a = requests.get("https://api.ipify.org?format=json").json().get("ip")
+    aaaa = requests.get("https://api6.ipify.org?format=json").json().get("ip")
+    ips = []
+
+    if(a.find(".") > -1):
+        ips.append({
+            "type": "A",
+            "ip": a
+        })
+
+    if(aaaa.find(":") > -1):
+        ips.append({
+            "type": "AAAA",
+            "ip": aaaa
+        })
+
+    return ips
+
+
+def commitRecord(ip):
+    exists = False
+
+    record = {
+        "type": ip["type"],
+        "name": config["cloudflare"]["subdomain"],
+        "content": ip["ip"],
+        "proxied": config["cloudflare"]["proxied"]
+    }
+
+    list = cf_api(
+        f"zones/{config['cloudflare']['zone_id']}/dns_records", "GET")
+
+    for r in list["result"]:
+        if (r["type"] == ip["type"] and r["name"] == config["cloudflare"]["subdomain"]):
+            exists = r["id"]
+
+    if(exists == False):
+        response = cf_api(
+            f"zones/{config['cloudflare']['zone_id']}/dns_records", "POST", {}, record)
+    else:
+        response = cf_api(
+            f"zones/{config['cloudflare']['zone_id']}/dns_records/{exists}", "PUT", {}, record)
+
+    print(response)
+    return True
+
+
+def cf_api(endpoint, method, headers={}, data=False):
+    headers = {
+        "X-Auth-Email": config['cloudflare']['account_email'],
+        "X-Auth-Key": config['cloudflare']['api_key'],
+        **headers
+    }
+
+    if(data == False):
+        response = requests.request(
+            method, f"https://api.cloudflare.com/client/v4/{endpoint}", headers=headers)
+    else:
+        response = requests.request(
+            method, f"https://api.cloudflare.com/client/v4/{endpoint}", headers=headers, json=data)
+
+    return response.json()
+
+
+for ip in getIPs():
+    commitRecord(ip)
